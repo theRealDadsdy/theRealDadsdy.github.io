c35877b2ad45005b9a280584544e7ac11a9a88d2
theRealDadsdy <94847478+theRealDadsdy@users.noreply.github.com>
2023-03-20 10:59:50 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,24 +3,13 @@ from flask import Flask, request
 from github import Github
 
 app = Flask(__name__)
-
+clicks = 0
 @app.route('/save-value', methods=['POST'])
 def save_value():
     value = request.form['value']
-    # authenticate to GitHub using an access token
-    g = Github("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")
-    # find the repository
-    repo = g.get_repo("theRealDadsdy/theRealDadsdy.github.io");
-    #create or get the file
-    file_name = "file.txt"
-    file_content = value
-    try:
-        file_path = f"path/to/{file_name}"
-        file_sha = repo.get_contents(file_path).sha
-        repo.update_file(file_path, "updated file", file_content, file_sha)
-    except:
-        repo.create_file(file_path, "created file", file_content)
-    return 'success'
+    if value:
+        clicks += 1
+    return clicks
 
 if __name__ == '__main__':
     app.run()
